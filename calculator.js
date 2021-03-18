cf34719a583106f319d30c61f5063063872430ac
chica25 <patricia@patriciaco.com>
2021-03-18 01:26:59 -0400

    Alter html attributes, Javascript functions and variables

--- calculator.js
+++ calculator.js
@@ -1,8 +1,18 @@
 function update(){
-    let bill = document.getElementById('your-bill').value
+    let bill = Number(document.getElementById('your-bill').value);
     let tipPercent = document.getElementById('tip-input').value;
-    let splitInput = document.getElementById('split-input').value
-    console.log(bill, tipPercent, splitInput)
+    let splitInput = document.getElementById('split-input').value;
+
+    let tipAmount = bill * (tipPercent / 100);
+    let tipByEachPerson = tipAmount / splitInput
+    let newBillByEachPerson = (bill + tipAmount ) / splitInput
+    
+    document.getElementById('tip-percent').innerHTML = tipAmount + '%'
+    document.getElementById('tip-value').innerHTML = tipAmount;
+    document.getElementById('total-with-tip').innerHTML = bill + tipAmount;
+    document.getElementById('split-value').innerHTML = splitInput
+    document.getElementById('bill-each').innerHTML = newBillByEachPerson
+    document.getElementById('tip-each').innerHTML = tipByEachPerson
 } 
 
 let containter = document.getElementById('container')
