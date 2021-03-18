8d0bd5396a4893d5bc48036b3e3cea8b4b181db2
chica25 <patricia@patriciaco.com>
2021-03-17 22:51:48 -0400

    Implement click event

---
+++ calculator.js
@@ -0,0 +1,12 @@
+
+function update(){
+    let bill = document.getElementById('your-bill').value
+    let tipPercent = document.getElementById('your-bill').value;
+    let splitBill = document.getElementById('your-bill').value
+    // console.log(bill, tipPercent, splitBill)
+}   
+
+
+
+
+// bill.addEventListener('click', () => {
